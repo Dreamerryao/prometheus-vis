c3b728d2ef9df26137dfc1afaeea86a5bf695355
13516055947 <3273395822@qq.com>
2022-08-09 20:07:31 +0800

    Revert "feat: 日期问题"
    This reverts commit 0eb5dd79d17fd9d0c1c8fa7e505aee5d98496e96.

--- pages/dashboard/sys/index.js
+++ pages/dashboard/sys/index.js
@@ -188,7 +188,7 @@ const DemoPie1 = () => {
       offset: '-30%',
       content: ({ percent }) => `${(percent * 100).toFixed(0)}%`,
       style: {
-        fontSize: 17,
+        fontSize: 20,
         textAlign: 'center',
       },
     },
@@ -238,7 +238,7 @@ const DemoPie2 = () => {
       offset: '-30%',
       content: ({ percent }) => `${(percent * 100).toFixed(0)}%`,
       style: {
-        fontSize: 17,
+        fontSize: 20,
         textAlign: 'center',
       },
     },
@@ -251,44 +251,22 @@ const DemoPie2 = () => {
   return <Pie {...config} />;
 };
 const DemoPie3 = () => {
-  const [data1, setData1] = useState([])
-    const [data2, setData2] = useState([])
-    useEffect(()=>{
-        http.get("/v1/api/error/js")
-        .then(e=>{
-            console.log(e)
-            e.data.forEach(i=>{
-                i.key = i.id
-            })
-            setData1(e.data)
-        })
-    }, [])
-    useEffect(()=>{
-      http.get("/v1/api/error/resource")
-      .then(e=>{
-          console.log(e)
-          e.data.forEach(i=>{
-              i.key = i.id
-          })
-          setData2(e.data)
-      })
-  }, [])
   const data = [
-    /*{
+    {
       type: '系统异常',
-      value: 0,
+      value: 40,
     },
     {
       type: '网络异常',
-      value: 0,
-    },*/
-    {
-      type: 'JS异常',
-      value: data1.length,
+      value: 40,
     },
     {
-      type: '资源异常',
-      value: data2.length,
+      type: 'JS异常',
+      value: 20,
+    },
+    {
+      type: '应用异常',
+      value: 20,
     },
   ];
   const config = {
@@ -302,7 +280,7 @@ const DemoPie3 = () => {
       offset: '-30%',
       content: ({ percent }) => `${(percent * 100).toFixed(0)}%`,
       style: {
-        fontSize: 21,
+        fontSize: 20,
         textAlign: 'center',
       },
     },
@@ -315,33 +293,14 @@ const DemoPie3 = () => {
   return <Pie {...config} />;
 };
 const DemoPie4 = () => {
-  const [detail1, setDetail1] = useState([])
-  useEffect(()=>{
-    http.get("/v1/api/behavior")
-    .then(e=>{
-        console.log(e)
-        e.data.forEach(i=>{
-            i.key = i.id
-        })
-        setDetail1(e.data)
-    })
-    }, [])
-  var obj={};
-  var uv=[]
-  for(var i=0;i<detail1.length;i++){
-    if(!obj[detail1[i].uuid]){
-      obj[detail1[i].uuid]=1
-      uv.push(detail1[i].uuid)
-    }
-  }
   const data = [
     {
       type: 'PC',
-      value: detail1.length,
+      value: 2102,
     },
     {
       type: 'UV',
-      value: uv.length,
+      value: 324,
     },
   ];
   const config = {
@@ -381,65 +340,75 @@ const config1 = {
           "value": 1100
         },
         {
-          "time": "12时",
+          "time": "9时",
           "type": "pv",
           "value": 1450
         },
         {
-          "time": "12时",
+          "time": "9时",
           "type": "uv",
           "value": 850
         },
         {
-          "time": "16时",
+          "time": "10时",
           "type": "pv",
           "value": 900
         },
         {
-          "time": "16时",
+          "time": "10时",
           "type": "uv",
           "value": 850
         },
         {
-          "time": "20时",
+          "time": "11时",
           "type": "pv",
           "value": 1600
         },
         {
-          "time": "20时",
+          "time": "11时",
           "type": "uv",
           "value": 500
         },
         {
-          "time": "22时",
+          "time": "12时",
           "type": "pv",
           "value": 1400
         },
         {
-          "time": "22时",
+          "time": "12时",
           "type": "uv",
           "value": 900
         },
         {
-          "time": "0时",
+          "time": "13时",
           "type": "pv",
           "value": 1400
         },
         {
-          "time": "0时",
+          "time": "13时",
           "type": "uv",
           "value": 900
         },
         {
-          "time": "4时",
+          "time": "14时",
           "type": "pv",
           "value": 900
         },
         {
-          "time": "4时",
+          "time": "14时",
           "type": "uv",
           "value": 850
         },
+        {
+          "time": "15时",
+          "type": "pv",
+          "value": 1700
+        },
+        {
+          "time": "15时",
+          "type": "uv",
+          "value": 600
+        }
     ],
     xField: 'time',
     yField: 'value',
@@ -448,140 +417,31 @@ const config1 = {
 }
 // 首页概览
 function Page() {
-  const [dataa, setData] = useState([])
-  const [detail, setDetail] = useState([])
-  const [detail1, setDetail1] = useState([])
-  const [detail2, setDetail2] = useState([])
-    useEffect(()=>{
-        http.get("/v1/api/error/js")
-        .then(e=>{
-            console.log(e)
-            e.data.forEach(i=>{
-                i.key = i.id
-            })
-            setData(e.data)
-        })
-}, [])
-    useEffect(()=>{
-      http.get("/v1/api/error/resource")
-      .then(e=>{
-          console.log(e)
-          e.data.forEach(i=>{
-              i.key = i.id
-          })
-          setDetail(e.data)
-        })
-}, [])
-  useEffect(()=>{
-    http.get("/v1/api/behavior")
-    .then(e=>{
-        console.log(e)
-        e.data.forEach(i=>{
-            i.key = i.id
-        })
-        setDetail1(e.data)
-    })
-}, [])
-useEffect(()=>{
-  http.get("/v1/api/http")
-  .then(e1=>{
-      console.log(e1)
-      e1.data.forEach(i=>{
-          i.key = i.id
-      })
-      setDetail2(e1.data)
-  })
-}, [])
-var qq=0;
-for( var i=0;i< detail2.length;i++){
-  qq=Number(qq)+Number(detail2[i].duration);
-}
-var cgl=0;
-for(var i=0;i<detail2.length;i++){
-  if(detail2[i].success){
-    cgl++;
-  }
-}
-  var obj={};
-  var uv=[]
-  for(var i=0;i<detail1.length;i++){
-    if(!obj[detail1[i].uuid]){
-      obj[detail1[i].uuid]=1
-      uv.push(detail1[i])
-    }
-  }
-  var time = new Date();
-  var year = time.getFullYear();
-  var mouth = time.getMonth()+1;
-  var day = time.getDate();
-  //日异常数
-  var dataatime=0;
-  for(var i=0;i<dataa.length;i++){
-  var date = new Date(dataa[i].timestamp*1000)
-  var y = date.getFullYear()
-  var m = date.getMonth()+1
-  var d = date.getDay()
-  if(year == y && mouth == m && day==d){
-    dataatime++;
-  }
-  }
-  for(var i=0;i<detail1.length;i++){
-    var date = new Date(detail1[i].timestamp*1000)
-    var y = date.getFullYear()
-    var m = date.getMonth()+1
-    var d = date.getDay()
-    if(year == y && mouth == m && day==d){
-      dataatime++;
-    }
-    }
-  //日请求数
-  var detail2time=0;
-  for(var i=0;i<detail2.length;i++){
-    var date = new Date(detail2[i].timestamp*1000)
-    var y = date.getFullYear()
-    var m = date.getMonth()+1
-    var d = date.getDay()
-    if(year == y && mouth == m && day==d){
-      detail2time++;
-    }
-    }
-  //日用户量
-  var uvtime=0;
-  for(var i=0;i<uv.length;i++){
-    var date = new Date(uv[i].timestamp*1000)
-    var y = date.getFullYear()
-    var m = date.getMonth()+1
-    var d = date.getDay()
-    if(year == y && mouth == m && day==d){
-      uvtime++;
-    }
-    }
-
     return <div className='system'>
       <div className='wrapper1'>
       <span className='shouye1'>
           <div className='s1'>总异常数：</div>
-          <div className='s2'>{dataa.length+detail.length}</div>
+          <div className='s2'>66</div>
           <div className='s3'>日异常数：</div>
-          <span className='s4'>{dataatime}</span>
+          <span className='s4'>66</span>
         </span>
         <span className='shouye1'>
           <div className='s1'>网络请求个数：</div>
-          <div className='s2'>{detail2.length}</div>
+          <div className='s2'>666</div>
           <div className='s3'>日请求数：</div>
-          <span className='s4'>{detail2time}</span>
+          <span className='s4'>66</span>
         </span>
         <span className='shouye1'>
           <div className='s1'>异常占比：</div>
-          <div className='s2'>{Math.round(dataa.length/(detail2.length+20)*100)}%</div>
+          <div className='s2'>66%</div>
           <div className='s3'>日异常占比：</div>
           <span className='s4'>66</span>
         </span>
         <span className='shouye1'>
-          <div className='s1'>用户个数</div>
-          <div className='s2'>{uv.length}</div>
-          <div className='s3'>日用户量：</div>
-          <span className='s4'>{uvtime}</span>
+          <div className='s1'>访问量</div>
+          <div className='s2'>66</div>
+          <div className='s3'>日访问量：</div>
+          <span className='s4'>66</span>
         </span>
       </div>
       <div className='wrapper'>
@@ -590,7 +450,7 @@ for(var i=0;i<detail2.length;i++){
                 <div className='behavior-count2'>
                     <div className='behavior-count-card count-card2'>
                         <div className='count-card-title2'>请求（ms）</div>
-                        <div className='count-card-value2'>{Math.round(qq/detail2.length)}ms</div>
+                        <div className='count-card-value2'>2102ms</div>
                     </div>
                     <div className='behavior-count-card count-card2'>
                         <div className='count-card-title2'>响应（ms）</div>
@@ -598,7 +458,7 @@ for(var i=0;i<detail2.length;i++){
                     </div>
                     <div className='behavior-count-card count-card2'>
                         <div className='count-card-title2'>成功率</div>
-                        <div className='count-card-value2'>{Math.round(cgl/(detail2.length)*100)}%</div>
+                        <div className='count-card-value2'>32%</div>
                     </div>
                 </div>
                 <div className='dempdualaxes'>
@@ -614,37 +474,31 @@ for(var i=0;i<detail2.length;i++){
             <div className='title1'>设备与异常类型</div>
             <div className='container'>
                 <div className='behavior-count1'>
-                    <div className='D'>
                     <div className='behavior-count-card count-card1'>
                         <div className='count-card-title1'>浏览器占比</div>
                         <div className='d1'>
                         <DemoPie1 />
                         </div>
                     </div>
-                    </div>
                 </div>
             </div>
             <div className='container'>
                 <div className='behavior-count1'>
-                    <div className='D'>
                     <div className='behavior-count-card count-card1'>
                         <div className='count-card-title1'>操作系统占比</div>
                         <div className='d1'>
                         <DemoPie2 />
                         </div>
                     </div>
-                    </div>
                 </div>
             </div>
-            <div className='container1'>
+            <div className='container'>
                 <div className='behavior-count1'>
-                    <div className='D'>
                     <div className='behavior-count-card count-card1'>
                         <div className='count-card-title1'>异常类型占比</div>
                         <div className='d1'>
                         <DemoPie3 />
                         </div>
-                    </div>
                     </div>
                 </div>
             </div>
@@ -657,11 +511,11 @@ for(var i=0;i<detail2.length;i++){
                 <div className='behavior-count'>
                     <div className='behavior-count-card count-card'>
                         <div className='count-card-title'>pv总数：</div>
-                        <div className='count-card-value'>{detail1.length}</div>
+                        <div className='count-card-value'>2102</div>
                     </div>
                     <div className='behavior-count-card count-card'>
                         <div className='count-card-title'>uv总数：</div>
-                        <div className='count-card-value'>{uv.length}</div>
+                        <div className='count-card-value'>324</div>
                     </div>
                 </div>
                 <div className='d2'>
@@ -701,7 +555,7 @@ for(var i=0;i<detail2.length;i++){
           }
             .wrapper .title1{
               color: black;
-              width: 505px;
+              width: 490px;
               margin-top: 0px;
               font-size: 18px;
               text-align: center;
@@ -746,15 +600,6 @@ for(var i=0;i<detail2.length;i++){
                 justify-content: space-between;
                 flex-wrap: wrap;
             }
-            .wrapper .container1{
-              margin-right: 20px;
-              width: 100%;
-              display: flex;
-              padding: 20px;
-              box-sizing: border-box;
-              justify-content: space-between;
-              flex-wrap: wrap;
-          }
             .wrapper2 .container{
               width: 100%;
               display: flex;
@@ -815,7 +660,7 @@ for(var i=0;i<detail2.length;i++){
             }
             .count-card1{
               background: white;
-              width: 100%;
+              width: 320px;
               height: 318px;
               padding: 10px;
               -webkit-box-sizing: border-box;
@@ -928,9 +773,6 @@ for(var i=0;i<detail2.length;i++){
           }
           .D4{
             width: 125%;
-          }
-          .D{
-            width: 199%
           }
         `}</style>
     </div>
